504c9664102da3684eeb49d7684637f65b51c7d1
Pratham <88399872+Pratham1001@users.noreply.github.com>
2023-03-27 11:37:29 +0530

    practical 1 q.1

---
+++ Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            {
+                int month = int.Parse(textBox1.Text);
+                int day = int.Parse(textBox2.Text);
+                int year = int.Parse(textBox3.Text);
+                if (month * day == year % 100)
+                {
+                    MessageBox.Show("month is magic");
+                }
+
+                else
+                { MessageBox.Show("not a magic month"); }
+            }
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+        }
+    }
+}
